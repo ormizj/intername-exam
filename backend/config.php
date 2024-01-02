2da2bb66256ed5ea3db1d806bfebc05b8fdd5ca8
spiderpig60 <spiderpig60@gmail.com>
2024-01-02 15:48:08 +0200

    manually added config.php this file is supposed to be private, but for demo purposes uploading it to the github

---
+++ backend/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// general variables
+$CERTIFICATION_URL = "C:/tools/php83/extras/ssl/cacert.pem";
+
+// database variables
+$SQL_HOST = '127.0.0.1:3306';
+$SQL_USER = 'media_supreme';
+$SQL_PASS = '1234';
+$SQL_DATABASE = 'leads_db';
+
+$INIT_URL = 'https://jsonplaceholder.typicode.com/users'; // used for creating the database dummy data
